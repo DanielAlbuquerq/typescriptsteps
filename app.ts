e85cc7eb2a49dc54051226bbc295e8d31dcc9f7d
Daniel Mendes de Albuquerque <daniel_developer@hotmail.com>
2024-04-20 21:24:15 -0300

    first commit

---
+++ app.ts
@@ -0,0 +1,22 @@
+// function add(n1: number, n2: number) {
+//   return n1 + n2
+// }
+
+// function printResult(num: number) {
+//   console.log("Result: " + num)
+// }
+
+// printResult(add(2, 1))
+
+// let combineValues: (a: number, b: number) => number
+
+// combineValues = add
+// combineValues = printResult
+
+// // combineValues = 5;
+// console.log(combineValues(4, 8))
+
+function generateError(message: string, code: number) {
+  throw { message: message, errorCode: code }
+}
+generateError("e", 500)
