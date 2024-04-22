f5091790b38998aaa0632071b2832ece8bae6e7b
Daniel Mendes de Albuquerque <daniel_developer@hotmail.com>
2024-04-21 23:36:56 -0300

    boolean and conditional

--- basics.ts
+++ basics.ts
@@ -8,4 +8,32 @@ const number1 = "5"
 const number2 = 2.8
 
 const result = add(number1, number2)
-console.log(result)
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Instead of using conditionals to validate inputs at runtime
+// We can check and avoid errors during development with TypeScript
+function add(n1: number, n2: number) {
+  // if (typeof n1 !== "number" || typeof n2 !== "number") {
+  //   throw new Error("Incorrect input")
+  // }
+  return n1 + n2
+}
+const number1 = 5
+const number2 = 2.8
+const result = add(number1, number2)
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Working with boolean value
+function add(n1: number, n2: number, showResult: boolean) {
+  if (showResult) {
+    console.log(n1 + n2)
+  } else {
+    return n1 + n1
+  }
+}
+
+const number1 = "5"
+const number2 = 2.8
+const printResult = true
+const result = add(number1, number2, printResult)
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
