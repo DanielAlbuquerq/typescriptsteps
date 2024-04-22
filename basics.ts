365fcf61fc65deb98fe0c7ae93d886080cad7e75
Daniel Mendes de Albuquerque <daniel_developer@hotmail.com>
2024-04-22 12:59:57 -0300

    assignments and inferences

--- basics.ts
+++ basics.ts
@@ -24,16 +24,52 @@ const result = add(number1, number2)
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 //Working with boolean value
-function add(n1: number, n2: number, showResult: boolean) {
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  const result = n1 + n2
   if (showResult) {
-    console.log(n1 + n2)
+    console.log(phrase + result)
   } else {
     return n1 + n1
   }
 }
 
-const number1 = "5"
+const number1 = 5
 const number2 = 2.8
 const printResult = true
-const result = add(number1, number2, printResult)
+const resultPhrase = "Result is: "
+
+const result = add(number1, number2, printResult, resultPhrase)
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Working with Type assignment and inference
+//Here I assigned the type of number1 and resultPhrase variables and made some inferences
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  const result = n1 + n2
+  if (showResult) {
+    console.log(phrase + result)
+  } else {
+    return n1 + n1
+  }
+}
+
+let number1: number
+number1 = "s"
+number1 = 5
+
+console.log(number1)
+
+const number2 = 2.8
+const printResult = true
+
+let resultPhrase = "Result is: "
+resultPhrase = 3
+
+console.log(typeof resultPhrase)
+if (typeof resultPhrase === "number") {
+  console.log("Variable type number: " + resultPhrase)
+} else {
+  console.log("Variable type: " + typeof resultPhrase)
+}
+
+const result = add(number1, number2, printResult, resultPhrase)
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
