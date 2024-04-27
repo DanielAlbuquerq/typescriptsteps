2333ba06995c42f24e917e5e33983a651f36ec94
Daniel Mendes de Albuquerque <daniel_developer@hotmail.com>
2024-04-26 22:10:08 -0300

    Union Type

--- basics.ts
+++ basics.ts
@@ -1,30 +1,30 @@
 //making TypeScript type assignments for javascript does not concatenate string with number
 //In this scenario we would use the '+' signal to transform the parameters into a number
-function add(n1: number, n2: number) {
+function add1(n1: number, n2: number) {
   return n1 + n2
 }
-
 const number1 = "5"
-const number2 = 2.8
+const number1_2 = 2.8
 
-const result = add(number1, number2)
+//intentional error
+const result1 = add1(number1, number1_2)
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 //Instead of using conditionals to validate inputs at runtime
 // We can check and avoid errors during development with TypeScript
-function add(n1: number, n2: number) {
+function add2(n1: number, n2: number) {
   // if (typeof n1 !== "number" || typeof n2 !== "number") {
   //   throw new Error("Incorrect input")
   // }
   return n1 + n2
 }
-const number1 = 5
-const number2 = 2.8
-const result = add(number1, number2)
+const number2 = 5
+const number2_1 = 2.8
+const result2 = add2(number2, number2_1)
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 //Working with boolean value
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+function add3(n1: number, n2: number, showResult: boolean, phrase: string) {
   const result = n1 + n2
   if (showResult) {
     console.log(phrase + result)
@@ -33,17 +33,17 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
   }
 }
 
-const number1 = 5
-const number2 = 2.8
+const number3 = 5
+const number3_1 = 2.8
 const printResult = true
 const resultPhrase = "Result is: "
 
-const result = add(number1, number2, printResult, resultPhrase)
+const result3 = add3(number3, number3_1, printResult, resultPhrase)
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 //Working with Type assignment and inference
 //Here I assigned the type of number1 and resultPhrase variables and made some inferences
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+function add4(n1: number, n2: number, showResult: boolean, phrase: string) {
   const result = n1 + n2
   if (showResult) {
     console.log(phrase + result)
@@ -52,24 +52,28 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
   }
 }
 
-let number1: number
-number1 = "s"
-number1 = 5
+let number4: number
+
+//intentional error
+number4 = "s"
+number4 = 5
 
 console.log(number1)
 
-const number2 = 2.8
-const printResult = true
+const number4_1 = 2.8
+const printResult4 = true
 
-let resultPhrase = "Result is: "
-resultPhrase = 3
+let resultPhrase4 = "Result is: "
 
-console.log(typeof resultPhrase)
-if (typeof resultPhrase === "number") {
-  console.log("Variable type number: " + resultPhrase)
+//intentional error
+resultPhrase4 = 3
+
+console.log(typeof resultPhrase4)
+if (typeof resultPhrase4 === "number") {
+  console.log("Variable type number: " + resultPhrase4)
 } else {
-  console.log("Variable type: " + typeof resultPhrase)
+  console.log("Variable type: " + typeof resultPhrase4)
 }
 
-const result = add(number1, number2, printResult, resultPhrase)
+const result4 = add4(number4, number4_1, printResult4, resultPhrase4)
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
--- objectTypes.ts
+++ objectTypes.ts
@@ -24,7 +24,7 @@ const person2: {
   hobbies: ["workout", "yoga", "play games"],
 }
 
-//To use mix array we need to type as any
+//To use multiple type in array we need to type as any
 let favoriteActivities: any[]
 favoriteActivities = ["programming", 1]
 
@@ -33,7 +33,7 @@ for (const hobby of person2.hobbies) {
   console.log(typeof person2.name)
 
   //this console.log is based on length of the array.
-  console.log(pperson.hobbies[0])
+  console.log(person2.hobbies[0])
 }
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -54,6 +54,7 @@ const person3: {
 }
 
 console.log(person3.role[1])
+//intentional error
 person3.role[1] = 2
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -84,3 +85,36 @@ if (person4.role === Role.AUTHOR) {
 if (person4.role === Role.ADMIN) {
   console.log("is Admin")
 }
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Union Types
+function combine(input1: number, input2: number) {
+  const result = input1 + input2
+  return result
+}
+//intentional error
+console.log(combine("Daniel ", "Ana"))
+//-------
+
+//Now the Combine1 Function can accept union types
+function combine1(input1: number | string, input2: number | string) {
+  let result: any
+
+  // I noticed that we commonly need to implement a runtime check
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2
+    console.log("type number - Total: " + result)
+  } else {
+    result = input1.toString() + " " + input2.toString()
+  }
+  return result
+}
+
+const combinedAges = combine1(30, 26)
+console.log(combinedAges)
+
+const combinedName = combine1("Daniel", "Mendes")
+console.log(combinedName)
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++
