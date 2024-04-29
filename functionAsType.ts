32c3cb7d35684674469f89f79f71bad706f9051a
Daniel Mendes de Albuquerque <daniel_developer@hotmail.com>
2024-04-29 20:57:40 -0300

    functionAsType

--- functionAsType.ts
+++ functionAsType.ts
@@ -37,3 +37,14 @@ combineValues2 = add5
 combineValues2 = printResult2
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2
+  cb(result)
+}
+
+addAndHandle(10, 20, (result) => {
+  console.log(result)
+})
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
