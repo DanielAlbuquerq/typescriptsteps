85504caa6662d270802bf1d9d60a6f14b2c3c067
Daniel Mendes de Albuquerque <daniel_developer@hotmail.com>
2024-05-02 23:40:55 -0300

    arrowFunctions 1

---
+++ src/arrowFunctions.ts
@@ -0,0 +1,8 @@
+const add11 = (a: number, b: number) => a + b
+
+const printOutput = (output: string | number) => {
+  console.log(output)
+}
+
+printOutput("Number but string")
+console.log(add11(2, 5))
