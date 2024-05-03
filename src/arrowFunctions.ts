bee7ea7e71ac9e2ee94300a14c2b9da52eb90afd
Daniel Mendes de Albuquerque <daniel_developer@hotmail.com>
2024-05-02 23:57:02 -0300

    arrowFunctions 1.1

--- src/arrowFunctions.ts
+++ src/arrowFunctions.ts
@@ -1,7 +1,18 @@
 const add11 = (a: number, b: number) => a + b
-
 const printOutput = (output: string | number) => {
   console.log(output)
+}
+
+//A more concise syntax of 'printOutput' function:
+const printOutput2: (a: number | string) => void = (output) =>
+  console.log(output)
+printOutput2(2)
+
+const button = document.querySelector("button")
+
+if (button) {
+  //using arrow function, short and concise syntax
+  button.addEventListener("click", (event) => console.log(event))
 }
 
 printOutput("Number but string")
