32cebf8f7aa5f005105015f38dbd333f9dd9b522
Daniel Mendes de Albuquerque <daniel_developer@hotmail.com>
2024-05-04 13:29:17 -0300

    OOP concepts

---
+++ src/ClassesAndInterface/firstClass.ts
@@ -0,0 +1,64 @@
+class Department {
+  name: string
+  localNumber: number
+
+  constructor(n: string, a: number) {
+    this.name = n
+    this.localNumber = a
+  }
+}
+const accounting = new Department("Accounting", 13)
+accounting.name = "IT"
+
+console.log(accounting)
+console.log(accounting.name)
+//==================================================
+
+//Constructor function with this in method
+class Department2 {
+  name: string
+
+  constructor(n: string) {
+    this.name = n
+  }
+  describe(this: Department2) {
+    console.log("Department2: " + this.name)
+  }
+}
+
+const financial1 = new Department2("financial").describe()
+
+//===================================================
+//++++++++++++++++ "Private" and "Public" Access.
+class Department3 {
+  public name: string
+  private employees: string[] = []
+
+  constructor(n: string) {
+    this.name = n
+  }
+  describe(this: Department3) {
+    console.log("Department3: " + this.name)
+  }
+  addEmployee(employee: string) {
+    this.employees.push(employee)
+  }
+  printEmployeeInformation() {
+    console.log(this.employees.length)
+    console.log(this.employees)
+  }
+}
+
+const financial2 = new Department3("financial2")
+
+financial2.addEmployee("Daniel")
+financial2.addEmployee("David")
+
+financial2.describe()
+financial2.printEmployeeInformation()
+
+//employees are assigned as private, we can not set new values outside the class.
+financial2.employees[2] = "Luis"
+console.log(financial2.employees)
+
+//======================================================
