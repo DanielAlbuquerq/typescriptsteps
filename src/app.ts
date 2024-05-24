52322c03c481fa7cf31b749520643db39765221a
Daniel Mendes de Albuquerque <daniel_developer@hotmail.com>
2024-05-24 01:15:22 -0300

    dom elements 1.1

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ class ProjectInput {
   descriptionInputElement: HTMLInputElement
   peopleInputElement: HTMLInputElement
 
-  constructor() {
+  constructor() 
     this.templateElement = document.getElementById(
       "project-input"
     )! as HTMLTemplateElement
@@ -15,9 +15,22 @@ class ProjectInput {
     const importedNode = document.importNode(this.templateElement.content, true)
     this.element = importedNode.firstElementChild as HTMLFormElement
     this.element.id = "user-input"
+
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement
+    
+    this.configure()
     this.attach()
+
+  private SubmitHandler(e: Event){
+    e.preventDefault();
+    console.log(this.titleInputElement.value)
   }
 
+  private configure(){
+    this.element.addEvenListener('submit', this.submitHandler)
+  } 
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element)
   }
